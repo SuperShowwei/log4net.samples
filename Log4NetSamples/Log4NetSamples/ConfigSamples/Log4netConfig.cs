cc5936acea979d133b8e2f96031ea9098e50bc8e
supershowwei <supershowwei@gmail.com>
2016-05-02 13:32:37 +0800

    add samples

---
+++ Log4NetSamples/Log4NetSamples/ConfigSamples/Log4netConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using log4net;
+using log4net.Config;
+
+//[assembly: log4net.Config.XmlConfigurator( ConfigFile = "Log4netConfig", Watch = true )]
+namespace NewsSmith.App_Start
+{
+    public class Log4netConfig
+    {
+        private static ILog log = LogManager.GetLogger(typeof(Log4netConfig));
+
+        public static void Start()
+        {
+
+            var configFile = ""; // ConfigurationManager.AppSettings.Get("Log4netConfig");
+            if (!string.IsNullOrEmpty(configFile))
+            {
+                configFile = Environment.ExpandEnvironmentVariables(configFile);
+                log4net.GlobalContext.Properties["ApplicationName"] = Assembly.GetExecutingAssembly().GetName().Name;
+                XmlConfigurator.ConfigureAndWatch(new FileInfo(configFile));
+
+                if (log.IsDebugEnabled)
+                {
+                    log.DebugFormat("載入Log4net Config 設定檔 {0} ", configFile);
+                }
+            }
+
+            if (log.IsDebugEnabled)
+            {
+                log.DebugFormat("Log4Net 從 Log4netConfig 設定完成");
+            }
+        }
+    }
+}
---
+++ Log4NetSamples/Log4NetSamples/Log4NetTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Log4NetSamples
+{
+    [TestClass]
+    public class Log4NetTest
+    {
+        public Log4NetTest()
+        {
+            // Another watch log4net configuration 
+            // log4net.Config.XmlConfigurator.Configure();
+
+            // Set ApplicationName
+            log4net.GlobalContext.Properties["ApplicationName"] = "Log4NetSamples";
+        }
+        [TestMethod]
+        public void Test_LogMethod()
+        {
+            ILog log = LogManager.GetLogger(this.GetType());
+
+            log.Info("Hello World");
+            log.InfoFormat("Hello World {0}", DateTime.Now);
+        }
+    }
+}
